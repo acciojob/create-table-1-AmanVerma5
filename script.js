d0b262d6604fd282521a6ff047dbe8d25f309a23
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-24 08:37:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
+  var table = document.getElementById("sampleTable");
+  var newRow = table.insertRow(0); // Insert row at the top
+
+  var cell1 = newRow.insertCell(0); // Insert left cell
+  var cell2 = newRow.insertCell(1); // Insert right cell
+
+  cell1.innerHTML = "New Cell1"; // Set value for left cell
+  cell2.innerHTML = "New Cell2"; // Set value for right cell
   
 }
